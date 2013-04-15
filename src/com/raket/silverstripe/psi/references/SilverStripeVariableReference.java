297ed0c15660ce714d834235e397605667089dfa
Marcus Dalgren <marcus.dalgren@gmail.com>
2013-04-15 22:25:42 +0200

    Variable resolves

---
+++ src/com/raket/silverstripe/psi/references/SilverStripeVariableReference.java
@@ -0,0 +1,11 @@
+package com.raket.silverstripe.psi.references;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Marcus Dalgren
+ * Date: 2013-04-15
+ * Time: 22:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class SilverStripeVariableReference {
+}
--- src/com/raket/silverstripe/references/SilverStripeVariablePsiReferenceProvider.java
+++ src/com/raket/silverstripe/references/SilverStripeVariablePsiReferenceProvider.java
@@ -1,11 +1,23 @@
 package com.raket.silverstripe.references;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Marcus Dalgren
- * Date: 2013-04-15
- * Time: 22:24
- * To change this template use File | Settings | File Templates.
- */
-public class SilverStripeVariablePsiReferenceProvider {
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.PsiReferenceProvider;
+import com.intellij.util.ProcessingContext;
+import com.raket.silverstripe.psi.impl.SilverStripeVariableImpl;
+import com.raket.silverstripe.psi.references.SilverStripeVariableReference;
+import org.jetbrains.annotations.NotNull;
+
+public class SilverStripeVariablePsiReferenceProvider extends PsiReferenceProvider {
+	@NotNull
+	@Override
+	public PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+
+		// IF we touch down here, we've got a variable!
+		if (element instanceof SilverStripeVariableImpl) {
+			SilverStripeVariableImpl se = (SilverStripeVariableImpl) element;
+			return new PsiReference[]{new SilverStripeVariableReference(se)};
+		}
+		return PsiReference.EMPTY_ARRAY;
+	}
 }
